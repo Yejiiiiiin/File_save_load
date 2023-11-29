afa235eb0a9a7fef111572300550f4feae7434e5
Yejiiiiiin <k040214@naver.com>
2023-11-29 23:55:52 +0900

    test

---
+++ FileSaveLoad/FileTest3/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace FileTest3
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbMath = new System.Windows.Forms.TextBox();
+            this.tbKor = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbMath);
+            this.groupBox1.Controls.Add(this.tbKor);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(33, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 203);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "입력";
+            // 
+            // tbMath
+            // 
+            this.tbMath.Location = new System.Drawing.Point(116, 148);
+            this.tbMath.Name = "tbMath";
+            this.tbMath.Size = new System.Drawing.Size(295, 41);
+            this.tbMath.TabIndex = 5;
+            // 
+            // tbKor
+            // 
+            this.tbKor.Location = new System.Drawing.Point(116, 90);
+            this.tbKor.Name = "tbKor";
+            this.tbKor.Size = new System.Drawing.Size(295, 41);
+            this.tbKor.TabIndex = 4;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(116, 34);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(295, 41);
+            this.tbName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "수학";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "국어";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "이름";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtResult);
+            this.groupBox2.ForeColor = System.Drawing.Color.Red;
+            this.groupBox2.Location = new System.Drawing.Point(33, 257);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(444, 276);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "출력";
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtResult.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtResult.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtResult.Location = new System.Drawing.Point(34, 37);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(377, 222);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.Text = "결과";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(33, 561);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 41);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(196, 561);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(114, 41);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "열기";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(363, 561);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(114, 41);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "초기화";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 625);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbMath;
+        private System.Windows.Forms.TextBox tbKor;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ FileSaveLoad/FileTest3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileTest3
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFile();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadFile();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void SaveFile()
+        {
+            string name = tbName.Text;
+            int nKor = int.Parse(tbKor.Text);
+            int nMath = int.Parse(tbMath.Text);
+
+            StreamWriter sw = new StreamWriter("data.txt");
+
+            sw.WriteLine(name);
+            sw.WriteLine(nKor);
+            sw.WriteLine(nMath);
+
+            sw.Close();
+
+            PrintResult(tbName.Text, tbKor.Text, tbMath.Text);
+        }
+
+        public void PrintResult(string name, string kor, string math)
+        {
+            txtResult.Text = "";
+            txtResult.Text += "이름 : " + name + "\n";
+            txtResult.Text += "국어 : " + kor + "\n";
+            txtResult.Text += "수학 : " + math + "\n";
+
+            txtResult.Text += "=====================\n";
+        }
+
+        public void LoadFile()
+        {
+            if (!File.Exists("data.txt"))
+                return;
+
+            try
+            {
+                StreamReader sr = new StreamReader("data.txt");
+
+                string sName = sr.ReadLine();
+                string sKor = sr.ReadLine();
+                string sMath = sr.ReadLine();
+
+                sr.Close();
+
+                tbName.Text = sName;
+                tbKor.Text = sKor;
+                tbMath.Text = sMath;
+
+                PrintResult(sName, sKor, sMath);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void Clear()
+        {
+            txtResult.Text = "";
+
+            tbName.Text = "";
+            tbKor.Text = "";
+            tbMath.Text = "";
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            Clear();
+            LoadFile();
+        }
+    }
+}
